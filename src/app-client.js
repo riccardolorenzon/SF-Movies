5f485175efa5d9a0c32e1e7c47ec8e3251011937
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-09-02 13:25:59 +0200

    initial project structure

---
+++ src/app-client.js
@@ -0,0 +1,7 @@
+// src/app-client.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = () => {
+  ReactDOM.render(<div>Hello world</div>, document.getElementById('main'));
+};
