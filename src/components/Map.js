978476f42883bf2d627391bf2521dc072002191a
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-09-30 16:32:41 +0200

    caching

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 
-const coords = {
-  lat: 37.7577627,
-  lng: -122.4726194
-};
-
 export default class Map extends React.Component {
   constructor(props) {
     super(props);
-    this.onZoomChanged = this.onZoomChanged.bind(this)
+    this.state = {
+      markers: {},
+      zoom: 12,
+      lat: 37.7577627,
+      lng: -122.4726194
+    };
+    this.onBoundsChanged = this.onBoundsChanged.bind(this);
   }
 
   onMapCreated(map) {
@@ -18,10 +19,8 @@ export default class Map extends React.Component {
       disableDefaultUI: true
     });
   }
+
   onBoundsChanged() {
-    console.log('bound change');
-  }
-  onZoomChanged() {
     var bounds = this.gmap.getMap().getBounds();
     var center = bounds.getCenter();
     var north_east = bounds.getNorthEast();
@@ -49,11 +48,23 @@ export default class Map extends React.Component {
       var data = JSON.parse(text);
       data.forEach(function(element) {
         var myLatLng = {lat: element.loc[1], lng: element.loc[0]};
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: _this.gmap.getMap(),
-          title: element.title + ' - ' + element.location
-        });
+
+        var hashkey = myLatLng.lat + '_' + myLatLng.lng;
+        var markers = _this.state.markers;
+        if (!(hashkey in markers)) {
+          var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: _this.gmap.getMap(),
+            title: element.title + ' - ' + element.location
+          });
+          markers[hashkey] = 1;
+          _this.setState({
+            markers: markers,
+            lat: center.lat(),
+            lng: center.lng(),
+            zoom: _this.gmap.getMap().getZoom()
+          });
+        }
       });
     })
   }
@@ -63,14 +74,13 @@ export default class Map extends React.Component {
         ref={(googleMap) => this.gmap = googleMap}
         width={'800px'}
         height={'600px'}
-        lat={coords.lat}
-        lng={coords.lng}
-        zoom={12}
+        lat={this.state.lat}
+        lng={this.state.lng}
+        zoom={this.state.zoom}
         loadingMessage={'Movies will show up in a little while :)'}
         params={{v: '3.exp', key: 'AIzaSyCaAXe1p_-gIMrXlKuBDzfkJxJ8187GGD4'}}
         onMapCreated={this.onMapCreated}
-        onZoomChanged={this.onZoomChanged.bind(this)}>
-        onBoundsChanged={this.onBoundsChanged}
+        onBoundsChanged={this.onBoundsChanged.bind(this)}>
       </Gmaps>
     );
   }
